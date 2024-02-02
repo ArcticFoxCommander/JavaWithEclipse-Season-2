24cd600fd1a9ced7675460990119bcf7ab5bd555
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-02-02 14:33:27 -0800

    Getting a student's age

---
+++ src/unit1/StudentInfo.java
@@ -0,0 +1,15 @@
+package unit1;
+import java.util.Scanner;
+public class StudentInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Object = new Scanner(System.in);
+		System.out.println("Student Age: ");
+		
+		String Age = Object.nextLine();
+		System.out.println("Student's age is " + Age);
+		
+	}
+	
+}
