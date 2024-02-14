6864425ede746207fbb452ac2cb64c7ff6e6fcae
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-02-14 11:30:49 -0800

    Testing my new JAVA FAN :D

---
+++ src/unit1/Excercise2/Fan.java
@@ -0,0 +1,16 @@
+package unit1.Excercise2;
+
+class Fan {
+
+	private final int SLOW = 1;
+	private final int MEDIUM = 2;
+	private final int FAST = 3;
+	
+	fan() {
+		final int speed = SLOW;
+		final boolean on 
+	}
+	
+	
+	
+}
---
+++ src/unit1/Excercise2/TestFan.java
@@ -0,0 +1,13 @@
+package unit1.Excercise2;
+
+public class TestFan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Fan fan1 = new Fan();
+		Fan fan2 = new Fan();
+		
+	}
+
+}
