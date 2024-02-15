33ad721e7958c89ddb4ce651940ca41e3ad0b71e
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-02-15 09:01:34 -0800

    My fan is done

--- src/unit1/Excercise2/Fan.java
+++ src/unit1/Excercise2/Fan.java
@@ -2,15 +2,51 @@ package unit1.Excercise2;
 
 class Fan {
 
-	private final int SLOW = 1;
-	private final int MEDIUM = 2;
-	private final int FAST = 3;
+	public static int SLOW = 1, MEDIUM = 2, FAST = 3;
 	
-	fan() {
-		final int speed = SLOW;
-		final boolean on 
+	private int speed = SLOW;
+	private boolean on = false;
+	private double radius = 5;
+	private String colour = "blue";
+	
+	public Fan() {
+		
 	}
 	
+	public int getSpeed() {
+		return speed;
+	}
 	
+	public void setSpeed(int newSpeed) {
+		speed = newSpeed;
+	}
+
+	public boolean isOn() {
+		return on;
+	}
+
+	public void setOn(boolean trueOrFalse) {
+		this.on = trueOrFalse;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double newRadius) {
+		radius = newRadius;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String newColour) {
+		colour = newColour;
+	}
+	
+	public String toString() {
+		return "Speed: " + speed + "\n" + "Colour: " + colour + "\n" + "Radius: " + radius + "\n" + ((on) ? "The fan is on." : "The fan is off.");
+	}
 	
 }
--- src/unit1/Excercise2/TestFan.java
+++ src/unit1/Excercise2/TestFan.java
@@ -6,8 +6,17 @@ public class TestFan {
 		// TODO Auto-generated method stub
 		
 		Fan fan1 = new Fan();
+		fan1.setSpeed(Fan.FAST);
+		fan1.setRadius(10);
+		fan1.setColour("Yellow");
+		fan1.setOn(true);
+		System.out.println(fan1.toString());
 		Fan fan2 = new Fan();
-		
+		fan2.setSpeed(Fan.MEDIUM);
+		fan2.setRadius(5);
+		fan2.setColour("Blue");
+		fan2.setOn(false);
+		System.out.println(fan2.toString());
 	}
 
 }
