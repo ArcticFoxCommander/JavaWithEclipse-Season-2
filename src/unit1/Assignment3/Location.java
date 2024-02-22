3189b016d34466e071476d6e294757fbd83ea350
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-02-22 10:13:12 -0800

    WIP Location Calculating for Max Value

---
+++ src/unit1/Assignment3/Location.java
@@ -0,0 +1,9 @@
+package unit1.Assignment3;
+
+public class Location {
+
+	public int row = 0;
+	public int column = 0;
+	public int maxValue = 0;
+	
+}
---
+++ src/unit1/Assignment3/TestLocation.java
@@ -0,0 +1,10 @@
+package unit1.Assignment3;
+
+public class TestLocation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		java.util.Scanner input = new java.util.Scanner(System.in);
+	}
+
+}
