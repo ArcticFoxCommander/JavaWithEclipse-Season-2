dee46206e2f15c0ec8354986396f77543057d4a9
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-02-22 09:13:36 -0800

    Weird date shenanigans

---
+++ src/unit1/Excercise3/Question4.java
@@ -0,0 +1,17 @@
+package unit1.Excercise3;
+import java.util.Date;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date date = null;
+		n1(date);
+		System.out.println(date);
+	}
+	
+	public static void n1(Date date) {
+		date = new Date();
+	}
+
+}
---
+++ src/unit1/Excercise3/Question4_V3.java
@@ -0,0 +1,17 @@
+package unit1.Excercise3;
+import java.util.Date;
+
+public class Question4_V3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date date = new Date(1234567);
+		n1(date);
+		System.out.println(date.getTime());
+	}
+	
+	public static void n1(Date date) {
+		date.setTime(7654321);
+	}
+
+}
