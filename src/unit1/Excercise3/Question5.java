ecf1d4eb36a5719706770a92e0e7ebc098d18478
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-02-22 09:16:54 -0800

    Null Pointer and Fixed Initialization

---
+++ src/unit1/Excercise3/Question5.java
@@ -0,0 +1,12 @@
+package unit1.Excercise3;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		java.util.Date[] dates = new java.util.Date[10];
+		System.out.println(dates[0]);
+		System.out.println(dates[0].toString());
+	}
+
+}
---
+++ src/unit1/Excercise3/Question5_V2.java
@@ -0,0 +1,14 @@
+package unit1.Excercise3;
+import java.util.Date;
+
+public class Question5_V2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Date[] dates = new Date[10];
+        dates[0] = new Date();
+        System.out.println(dates[0]);
+        System.out.println(dates[0].toString());
+	}
+
+}
