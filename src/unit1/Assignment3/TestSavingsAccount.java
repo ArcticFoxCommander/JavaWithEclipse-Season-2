407e41f372f5393fc404377b9541d4c052739b19
ArticFoxCommander <williamlovesyt@gmail.com>
2024-02-24 02:32:55 -0800

    Update TestSavingsAccount.java
    Fixed interest calculations with a new input as well

--- src/unit1/Assignment3/TestSavingsAccount.java
+++ src/unit1/Assignment3/TestSavingsAccount.java
@@ -4,8 +4,17 @@ public class TestSavingsAccount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SavingsAccount saver1 = new SavingsAccount(2000.00);
-		SavingsAccount saver2 = new SavingsAccount(3000.00);
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		// For this EXCERCISE, INPUT saver 1 for 2000.00 and saver 2 for 3000.00
+		System.out.println("Enter the amount for saver1: ");
+		double amt1 = input.nextDouble();
+		System.out.println("Enter the amount for saver2: ");
+		double amt2 = input.nextDouble();
+		
+		input.close();
+		
+		SavingsAccount saver1 = new SavingsAccount(amt1);
+		SavingsAccount saver2 = new SavingsAccount(amt2);
 		
 		System.out.println("Saver 1 & 2 with 4% interest.");
 		SavingsAccount.modifyInterestRate(0.04);
@@ -14,7 +23,10 @@ public class TestSavingsAccount {
 		saver2.calculateMonthlyInterest();
 		System.out.printf("Month %d:%n $%.2f %n $%.2f%n",i , saver1.getSavingsBalance(), saver2.getSavingsBalance());
 		}
-
+		
+		saver1 = new SavingsAccount(amt1);
+		saver2 = new SavingsAccount(amt2);
+		
 		SavingsAccount.modifyInterestRate(0.05);
 
 		
