0461e70483bdd6e77333f1e93f49575a6e025930
ArticFoxCommander <williamlovesyt@gmail.com>
2024-02-23 23:37:44 -0800

    Location finder for numbers
    THIS TOOK AN HOUR TO MAKE FFFSSSFSFSIAFOIEJFOIWJFGOWHBIAEUPGBUAW3HTO9HW

--- src/unit1/Assignment3/Location.java
+++ src/unit1/Assignment3/Location.java
@@ -4,6 +4,32 @@ public class Location {
 
 	public int row = 0;
 	public int column = 0;
-	public int maxValue = 0;
+	public double maxValue = 0;
+
+	Location(int row, int column, double maxValue) {
+		this.row = row;
+		this.column = column;
+		this.maxValue = maxValue;
+	}
+	
+	// e = row, and f = column
+	
+	public static Location locateLargest(double[][] a) {
+		
+		int maxRow =0;
+		int maxColumn = 0;
+		double maxValue = a[0][0];
+		
+		for (int e = 0; e < a.length; e++) {
+			for (int f = 0; f < a[e].length; f++) {
+				if (a[e][f] > maxValue) {
+					maxValue = a[e][f];
+					maxRow = e;
+					maxColumn = f;
+				}
+			}
+		}
+		return new Location(maxRow, maxColumn, maxValue);
+	}
 	
 }
--- src/unit1/Assignment3/TestLocation.java
+++ src/unit1/Assignment3/TestLocation.java
@@ -5,6 +5,26 @@ public class TestLocation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		java.util.Scanner input = new java.util.Scanner(System.in);
+		System.out.println("Enter the number of rows: ");
+		int row = input.nextInt();
+		System.out.println("Enter the number of columns: ");
+		int column = input.nextInt();
+		
+		double[][] array = new double[row][column];
+		
+		System.out.println("Enter the array for " + row + " rows and " + column + " columns.");
+		for (int a = 0; a < row; a++) {
+			for (int b = 0; b < column; b++) {
+				array[a][b] = input.nextDouble();
+			}
+		}
+		
+		Location find = Location.locateLargest(array);
+		
+		System.out.println("The location of the largest element is " + find.maxValue + " at " + find.row + " , " + find.column);
+		
+		
+		input.close();
 	}
 
 }
