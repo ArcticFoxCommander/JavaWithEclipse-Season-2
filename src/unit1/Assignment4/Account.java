ae92ca75ce83714ed8443c3b91068e8f26c6f391
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-02-28 11:25:59 -0800

    Java Account
    with 'this'

---
+++ src/unit1/Assignment4/Account.java
@@ -0,0 +1,61 @@
+package unit1.Assignment4;
+
+import java.util.Date;
+
+public class Account {
+
+	private int id;
+	private double balance;
+	private double annualInterestRate;
+	private Date dateCreated;
+	
+	Account() {
+		this(0, 0.0, 0.0);
+	}
+
+	Account(int id, double balance, double annualInterestRate) {
+		// TODO Auto-generated constructor stub
+		this.id = id;
+		this.balance = balance;
+		this.annualInterestRate = annualInterestRate;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+
+	public Date dateCreated() {
+		Date dateCreated = new Date();
+		return dateCreated;
+	}
+	
+	double getMonthlyInterestRate() {
+		return annualInterestRate / 12;
+	}
+	
+	double getMonthlyInterest() {
+	
+	return getBalance() * (annualInterestRate / 1200);	
+	}
+	
+	public void withdraw(double withdraw) {
+		if (withdraw > getBalance()) {
+			System.out.println("The withdrawed amount exceeds your current balance for account user: " + getId());
+			System.exit(0);
+		}
+		else {
+		balance -= withdraw;
+		}
+	}
+	
+	public void deposit(double deposit) {
+		balance += deposit;
+	}
+	
+	
+}
---
+++ src/unit1/Assignment4/TestAccount.java
@@ -0,0 +1,27 @@
+package unit1.Assignment4;
+
+
+public class TestAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Account user = new Account(1122, 20000, 4.5); {
+			
+		}
+		double withdraw = 2500; user.withdraw(withdraw);
+		double deposit = 3000; user.deposit(deposit);
+		
+		System.out.printf("__________________________________%n%nWelcome to your JAVA Bank Account%n"
+							
+						    + "Date: %s%nID: %s%n"
+						    + "__________________________________%n%n"
+						    + "Monthly Interest: $%.2f%n"
+							
+							+ "Today you withdrew: $%.2f%n"
+							+ "Today you deposited: $%.2f%n"
+							+ "New Balance: $%.2f%n__________________________________", user.dateCreated(), user.getId(), user.getMonthlyInterest(), withdraw, deposit, user.getBalance());	
+		
+		}
+	}
+
+
