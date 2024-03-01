5a1cd96833a47bfa47f386aeaf1ef7bbaf814598
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-01 10:14:54 -0800

    Create Question6.java

---
+++ src/unit1/Excercise4/Question6.java
@@ -0,0 +1,14 @@
+package unit1.Excercise4;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Question6");
+		System.out.println("Question6 hmm");
+		System.out.println("Question6 aaa");
+		System.out.println("Question6 eee");
+		
+	}
+
+}
