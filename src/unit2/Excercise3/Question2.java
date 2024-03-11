52e39e40b4a6e472a90565bc8e8ad32855563f08
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-11 14:40:45 -0700

    Create Question2.java

---
+++ src/unit2/Excercise3/Question2.java
@@ -0,0 +1,19 @@
+package unit2.Excercise3;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i = Integer.valueOf("23");
+		Integer ii = Integer.valueOf(23);
+		Integer iii = Integer.parseInt("23", 8);
+		// Double d = new Double();
+		Double dd = Double.valueOf("23.45");
+		int iiii = (Integer.valueOf("23")).intValue();
+		double ddd = (Double.valueOf("23.4")).doubleValue(); 
+		int iiiii = (Double.valueOf("23.4")).intValue();
+		String s = (Double.valueOf("23.4")).toString(); 
+
+	}
+
+}
