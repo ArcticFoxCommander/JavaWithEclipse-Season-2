27bd28ca06b9692e162e79797ceaafe0b5a5285b
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-11 14:41:02 -0700

    Create Question7.java

---
+++ src/unit2/Excercise3/Question7.java
@@ -0,0 +1,12 @@
+package unit2.Excercise3;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Double x = Double.valueOf(3.5);
+		System.out.println(x.doubleValue());
+		System.out.println(x.compareTo(4.5));
+	}
+
+}
