b836a50e4d561c205e45689b751df1651f24957d
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-11 14:40:54 -0700

    Create Question5.java

---
+++ src/unit2/Excercise3/Question5.java
@@ -0,0 +1,15 @@
+package unit2.Excercise3;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Integer.parseInt("10"));
+		System.out.println(Integer.parseInt("10", 10));
+		System.out.println(Integer.parseInt("10", 16));
+		System.out.println(Integer.parseInt("11"));
+		System.out.println(Integer.parseInt("11", 10));
+		System.out.println(Integer.parseInt("11", 16));
+	}
+
+}
