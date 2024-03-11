f856465b24a4dbbf1269dfb7405474eac58987bc
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-11 14:40:49 -0700

    Create Question4.java

---
+++ src/unit2/Excercise3/Question4.java
@@ -0,0 +1,12 @@
+package unit2.Excercise3;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer x = Integer.valueOf(3);
+		System.out.println(x.intValue());
+		System.out.println(x.compareTo(4));
+	}
+
+}
