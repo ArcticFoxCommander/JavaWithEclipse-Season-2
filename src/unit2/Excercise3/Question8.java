a84a78e00f3be598a1c934279a7844f3ee50f2b6
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-11 14:41:10 -0700

    Create Question8.java

---
+++ src/unit2/Excercise3/Question8.java
@@ -0,0 +1,15 @@
+package unit2.Excercise3;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		java.math.BigInteger x = new java.math.BigInteger("3");
+		java.math.BigInteger y = new java.math.BigInteger("7"); 
+		java.math.BigInteger z = x.add(y); 
+		System.out.println("x is " + x);
+		System.out.println("y is " + y);
+		System.out.println("z is " + z);
+
+	}
+}
