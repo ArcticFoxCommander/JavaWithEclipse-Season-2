b5823dd2eb552937778d3d4ba6e3a4f9885dd8d4
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-12 13:25:38 -0700

    Update Question13.java

--- src/unit2/Excercise3/Question13.java
+++ src/unit2/Excercise3/Question13.java
@@ -3,21 +3,21 @@ package unit2.Excercise3;
 public class Question13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String s1 = "Welcome";
 		String s2 = "welcome";
 		
 		s2 = s1.replaceAll("e", "E");
 		
 		System.out.println(s1);
+		System.out.println(s2);
 		
 		String s3 = "Welcome to Java and HTML";
 		String[] token = s3.split(" ");
 		
-		s1 = token[0];
-		s2 = token[1];
+		String s4 = token[0];
+		String s5 = token[1];
 		
-		System.out.printf("%s%n%s%n%s%n", s1, s2, s3);
+		System.out.printf("%s%n%n%s%n%s%n", s3, s4, s5);
 	}
 
 }
