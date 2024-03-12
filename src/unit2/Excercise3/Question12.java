8df2e287d145a920839f95f2809afffc5abde6d9
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-12 13:17:04 -0700

    Create Question12.java

---
+++ src/unit2/Excercise3/Question12.java
@@ -0,0 +1,12 @@
+package unit2.Excercise3;
+
+public class Question12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Welcome to Java";
+		String s2 = s1.replace("o", "abc");
+		System.out.printf("%s%n%s%n", s1, s2);
+	}
+
+}
