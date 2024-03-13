b69751369b914704774caf0f3711bf5709049ed1
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-13 11:29:03 -0700

    Create Question3.java

---
+++ src/unit2/Excercise4/Question3.java
@@ -0,0 +1,16 @@
+package unit2.Excercise4;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder("Java is ok");
+		StringBuilder ssb = new StringBuilder("Emmanuel Ilunga");
+		StringBuilder sssb = new StringBuilder("Reverse");
+		sb.reverse();
+		ssb.reverse();
+		sssb.reverse();
+		System.out.printf("%s%n%s%n%s%n", sb, ssb, sssb);
+	}
+
+}
