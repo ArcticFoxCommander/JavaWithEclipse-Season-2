b9f16d2f6f8796525e6c58123bb2a1ab1ba3f307
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-13 11:28:59 -0700

    Create Question2.java

---
+++ src/unit2/Excercise4/Question2.java
@@ -0,0 +1,15 @@
+package unit2.Excercise4;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder("Java");
+		sb.append(" is ok");
+		String ssb;
+		ssb = sb.toString();
+		System.out.println(sb);
+		System.out.println(ssb);
+	}
+
+}
