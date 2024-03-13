455e5e09007d7a0f05f5a0607b24393b63b63647
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-13 10:42:31 -0700

    Create Question17.java

---
+++ src/unit2/Excercise3/Question17.java
@@ -0,0 +1,16 @@
+package unit2.Excercise3;
+
+public class Question17 {
+	private String text;
+	
+	public Question17(String s) {
+		// String text = s;
+		this.text = s;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Question17 question17 = new Question17("ABC");
+		System.out.println(question17.text.toLowerCase());
+	}
+
+}
