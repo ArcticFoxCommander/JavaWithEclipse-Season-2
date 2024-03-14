d67d25fe8fb4471aac0f756582ed14d3d1f0db0a
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-14 09:34:59 -0700

    Update TestCourse.java

--- src/unit2/TestCourse.java
+++ src/unit2/TestCourse.java
@@ -16,10 +16,11 @@ public class TestCourse {
 	    System.out.println("Number of students in course1: " + course1.getNumberOfStudents());
 	    String[] students = course1.getStudents();
 	    for (int i = 0; i < course1.getNumberOfStudents(); i++)
+	    	
 	      System.out.print(students[i] + ", ");
 	    
 	    System.out.println();
-	    System.out.print("Number of students in course2: \n" + course2.getNumberOfStudents());
+	    System.out.print("Number of students in course2: " + course2.getNumberOfStudents() + "\n");
 	    String[] sstudents = course2.getStudents();
 	    for (int i = 0; i < course2.getNumberOfStudents(); i++)
 	      System.out.print(sstudents[i] + ", ");
