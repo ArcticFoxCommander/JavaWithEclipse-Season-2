691152c444b28e284703b52d1461a9ba786b6ada
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-03-14 09:35:40 -0700

    Create Question4.java

---
+++ src/unit2/Excercise1/Question4.java
@@ -0,0 +1,12 @@
+package unit2.Excercise1;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyInteger integer = new MyInteger(5);
+		
+		System.out.println(integer);
+	}
+
+}
