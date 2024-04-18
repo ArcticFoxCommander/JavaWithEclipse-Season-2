5e66019f873aa105bf242455821ff275d6dc03ee
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-04-18 09:10:21 -0700

    DynamicBindingDemo

---
+++ src/unit3/Person.java
@@ -0,0 +1,7 @@
+package unit3;
+
+public class Person extends Object {
+	public String toString() {
+	    return "Person";
+	  }
+}
---
+++ src/unit3/Student.java
@@ -0,0 +1,7 @@
+package unit3;
+
+public class Student extends Person {
+	public String toString() {
+	    return "Student";
+	  }
+}
