40d5405edf1f65c07bf7a4a4b939f96dc7cf84e3
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-04-24 11:28:46 -0700

    Create Question2.java

---
+++ src/unit3/Excercise5/Question2.java
@@ -0,0 +1,20 @@
+package unit3.Excercise5;
+
+import java.util.ArrayList;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> list = new ArrayList<>();
+		
+		list.add("Denver");
+		list.add("Austin");
+		list.add("Hello");
+		//list.add(new java.util.Date());
+		String city = list.get(0);
+		list.set(3, "Dallas");
+		System.out.println(list.get(2));
+	}
+
+}
