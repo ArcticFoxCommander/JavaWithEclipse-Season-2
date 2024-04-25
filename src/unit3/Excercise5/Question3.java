829e3ec037bfca143b62e530d4de5ebb22171d18
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-04-25 08:59:43 -0700

    Update Question3.java

--- src/unit3/Excercise5/Question3.java
+++ src/unit3/Excercise5/Question3.java
@@ -14,11 +14,33 @@ public class Question3 {
 		list.add("Houston");
 		list.add("Dallas");
 		
-		for (int i = 0; i < list.size(); i++) {
+//		for (int i = 0; i < list.size(); i++) {
+//			list.remove("Dallas");
+//		}
+		
+		while (list.contains("Dallas")) {
 			list.remove("Dallas");
 		}
 		
 		System.out.println(list);
+		
+		ArrayList<String> cityList = new ArrayList<>();
+		
+	    cityList.add("Miami");
+	    cityList.add("Miami");
+	    cityList.add("Seoul");
+	    cityList.add("Miami");
+	    
+	    System.out.println("List size? " + cityList.size());
+//	    for (int n = 0; n < cityList.size(); n++ ) {
+//	  	      cityList.remove("Miami");
+//	    }
+	    
+		while (cityList.contains("Miami")) {
+			cityList.remove("Miami");
+		}
+	    
+	    System.out.println(cityList);
 	}
 
 }
