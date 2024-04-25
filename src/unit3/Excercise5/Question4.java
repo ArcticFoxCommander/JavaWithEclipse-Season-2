e13cdbba89d8753dd119286f8bc4f6f2715a7afa
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-04-25 09:16:16 -0700

    Create Question4.java

---
+++ src/unit3/Excercise5/Question4.java
@@ -0,0 +1,19 @@
+package unit3.Excercise5;
+
+import java.util.ArrayList;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+//		list.remove(1);
+		list.remove(Integer.valueOf(1));
+		System.out.println(list);
+	}
+
+}
