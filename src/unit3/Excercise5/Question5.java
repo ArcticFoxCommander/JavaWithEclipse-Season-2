969dff3ca13661188879f588b44693976cbd0b23
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-04-25 09:16:28 -0700

    Create Question5.java

---
+++ src/unit3/Excercise5/Question5.java
@@ -0,0 +1,16 @@
+package unit3.Excercise5;
+
+import java.util.ArrayList;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Double> list = new ArrayList<>();
+		
+		list.add(1.0);
+		list.add((double) 2);
+		System.out.println(list);
+	}
+
+}
