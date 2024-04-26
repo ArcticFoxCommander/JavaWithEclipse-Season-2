cfd0f055db8f64598b4847711365c7e2bb3fd9e6
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-04-26 12:54:02 -0700

    Create Question1.java

---
+++ src/unit3/Excercise6/Question1.java
@@ -0,0 +1,16 @@
+package unit3.Excercise6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] array = {3, 5, 95, 4, 15, 34, 3, 6, 5};
+		ArrayList<Integer> list = new ArrayList<>(Arrays.asList(array));
+		
+		System.out.println(list);
+	}
+
+}
