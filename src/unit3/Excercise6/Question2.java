d95aa30f18d79d1f6bb41cb8925baa5937f584cb
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-04-26 13:02:32 -0700

    Create Question2.java

---
+++ src/unit3/Excercise6/Question2.java
@@ -0,0 +1,13 @@
+package unit3.Excercise6;
+
+import java.util.Arrays;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] array = {2, 3, 4 , 1, 23, 56};
+		System.out.println(java.util.Collections.max(Arrays.asList(array)));
+	}
+
+}
