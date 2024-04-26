e30d339df15cad1581cccb4cd9f9773d908ee546
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-04-26 13:24:54 -0700

    Create Question3.java

---
+++ src/unit3/Excercise6/Question3.java
@@ -0,0 +1,17 @@
+package unit3.Excercise6;
+
+import java.util.Scanner;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a sequence of numbers, add 0 at the end to stop inputting.");
+		
+	}
+
+}
+
+class 
