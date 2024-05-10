309b3bb52bfc437b0c6f2fe0697b4de97b73bf18
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-05-10 11:28:22 -0700

    Create Question3.java

---
+++ src/unit4/Excercise1/Question3.java
@@ -0,0 +1,12 @@
+package unit4.Excercise1;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     long value = Long.MAX_VALUE + 1; 
+	     System.out.println(value); 
+
+	}
+
+}
