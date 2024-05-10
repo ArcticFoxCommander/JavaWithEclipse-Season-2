9d05e30bd1441575c64106b18d56a62e993354fe
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-05-10 11:28:11 -0700

    Create Question2.java

---
+++ src/unit4/Excercise1/Question2.java
@@ -0,0 +1,13 @@
+package unit4.Excercise1;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// System.out.println(1 / 0); 
+	    // System.out.println(1.0 / 0);
+	    System.out.println(1 / 0.0);
+
+	}
+
+}
