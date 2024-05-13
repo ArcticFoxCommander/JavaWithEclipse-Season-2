5d2771058a0b4cb6c760382ac90314f605a0c5ba
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-05-13 14:38:15 -0700

    Update Question7.java

--- src/unit4/Excercise1/Question7.java
+++ src/unit4/Excercise1/Question7.java
@@ -5,26 +5,27 @@ public class Question7 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		java.util.Scanner input = new java.util.Scanner(System.in);
-		
+
 		int n = 0;
 		int m = 0;
-		
+
 		boolean good = false;
-		
+
 		while (!good) {
-		try {
-			System.out.print("Input two integers: ");
-			n = input.nextInt();
-			m = input.nextInt();
-			
-			good = true;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println("Incorrect Input, try again: ");
-			input.nextLine();
+			try {
+				System.out.print("Input two integers: ");
+				n = input.nextInt();
+				m = input.nextInt();
+
+				good = true;
+				input.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("Incorrect Input, try again: ");
+				input.nextLine();
+			}
 		}
-	}
 		System.out.printf("The sum of %s and %s is %s.", n, m, n+m);
-}
+	}
 
 }
