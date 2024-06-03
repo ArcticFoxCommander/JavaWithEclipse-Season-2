a03837fdbca92c8101b32d00ce5469f7d42bb1c5
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-06-03 14:39:20 -0700

    Update Question1.java

--- src/unit4/Assignment1/Question1.java
+++ src/unit4/Assignment1/Question1.java
@@ -1,8 +1,27 @@
 package unit4.Assignment1;
 
+	class ExceptionA extends Exception {
+	    public ExceptionA(String message) {
+	        super(message);
+	    }
+	}
+	
+	class ExceptionB extends ExceptionA {
+	    public ExceptionB(String message) {
+	        super(message);
+	    }
+	}
+	
+	class ExceptionC extends ExceptionA {
+	    public ExceptionC(String message) {
+	        super(message);
+	    }
+	}
+
+
 public class Question1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		try {
 			throw new ExceptionC("Exception C");
