763e40181b99361abbce62fd84724e20eeb1056f
ArticFoxCommander <williamlovesyt@gmail.com>
2024-06-05 17:59:16 -0700

    Create Customer.java

---
+++ src/unit4/Assignment2/Customer.java
@@ -0,0 +1,49 @@
+package unit4.Assignment2;
+
+import java.util.Date;
+
+public class Customer {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private Date birthDate;
+	
+	Customer () {
+		this(-1, "", "", new Date());
+	}
+	
+	public Customer(int id, String firstName, String lastName, Date birthDate) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthDate = birthDate;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	
+}
