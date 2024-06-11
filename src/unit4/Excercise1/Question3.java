039d287d6d50e5cf5e55ab77ea0020bcc6c6085b
Fox <60055478+ArcticFoxCommander@users.noreply.github.com>
2024-06-11 14:33:00 -0700

    Update Question3.java

--- src/unit4/Excercise1/Question3.java
+++ src/unit4/Excercise1/Question3.java
@@ -4,9 +4,10 @@ public class Question3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	     long value = Long.MAX_VALUE + 1; 
-	     System.out.println(value); 
-
+	     int value = Integer.MAX_VALUE + 1; 
+	     System.out.println(value);
+	     long value2 = Long.MAX_VALUE + 1;
+	     System.out.println(value2);
 	}
 
 }
